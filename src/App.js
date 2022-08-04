8e48acc2b1f8172e35fc243315d8c8f57ad78756
unknown <samir.hireche@gmail.com>
2022-08-04 15:27:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMyMarvelBackend = async () => {
+      const response = await axios.get(
+        "https://marvel-seza.herokuapp.com/characters"
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchMyMarvelBackend();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Marvel</h1>
+
+      {isLoading === true ? (
+        <h2>En cours de chargement</h2>
+      ) : (
+        <>
+          {data.charactData.results.map((character) => {
+            return (
+              <div className="fiche">
+                <p className="charactername" key={character._id}>
+                  {character.name}
+                </p>
+
+                <p className="characterdescription">{character.description}</p>
+                <img
+                  className="picturecharacter"
+                  src={
+                    character.thumbnail.path +
+                    "." +
+                    character.thumbnail.extension
+                  }
+                  alt="Picturecharacter"
+                />
+              </div>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 }
