29f42ebedb73dbeec1f31c3a80b89a85c42613bf
unknown <samir.hireche@gmail.com>
2022-08-07 17:34:54 +0200

    stop

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -1,4 +1,35 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Character = () => {
-  return <p>marvel</p>;
+  const { characterId } = useParams();
+  const [dataCharacter, setDataCharacter] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      try {
+        const response = await axios.get(
+          `https://marvel-seza.herokuapp.com/comics/${characterId}`
+        );
+        setDataCharacter(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("error.message");
+      }
+    };
+    fetchCharacter();
+  }, [characterId]);
+
+  return isLoading ? (
+    <p>loading</p>
+  ) : (
+    <div>
+      {dataCharacter.mainData.map((comic, index) => {
+        return <div>{comic.title}</div>;
+      })}
+    </div>
+  );
 };
 export default Character;
